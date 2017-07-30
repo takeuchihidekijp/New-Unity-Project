309f976dab4529048d016059b4c2ee89baf7479d
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2017-07-30 21:17:10 +0900

    修正

---
+++ Assets/EnemyContoroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyContoroller : MonoBehaviour {
+
+    //Playerのオブジェクト
+    private GameObject Player;
+
+
+    // Use this for initialization
+    void Start () {
+
+        this.Player = GameObject.Find("Player");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 diff = Player.transform.position - this.transform.position;
+
+    }
+}
--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class EnemyManager : MonoBehaviour {
 
     //Prefabを入れる
     public GameObject EnemyPrefab;
