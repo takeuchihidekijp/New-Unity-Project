5c5525008bc9f10e6e3bcabcbabf0b3a0062a6d4
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2017-08-01 07:21:20 +0900

    敵の動きを実装。不具合あり

--- Assets/EnemyContoroller.cs
+++ Assets/EnemyContoroller.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EnemyContoroller : MonoBehaviour {
 
+    public float speed = 6f;            // The speed that the player will move at.
+
+
+    Vector3 movement;                   // The vector to store the direction of the Enemy's movement.
+    Rigidbody enemyRigidbody;          // Reference to the enemy's rigidbody.
+
     //Playerのオブジェクト
     private GameObject Player;
 
@@ -20,5 +26,42 @@ public class EnemyContoroller : MonoBehaviour {
 
         Vector3 diff = Player.transform.position - this.transform.position;
 
+        if (Mathf.Abs(diff.x) < Mathf.Abs(diff.z))
+        {
+            if(diff.z > 0)
+            {
+                //下方向
+                enemyRigidbody.MovePosition(this.transform.position + new Vector3(0, 0, -1));
+                enemyRigidbody.MoveRotation(Quaternion.Euler(0, 0, 0));
+            }
+            else
+            {
+                // 上方向
+                enemyRigidbody.MovePosition(this.transform.position + new Vector3(0, 0, 1));
+                enemyRigidbody.MoveRotation(Quaternion.Euler(0, 0, 0));
+            }
+
+
+        }
+        else
+        {
+            // X軸の距離がZ軸の距離より大きい
+            if(diff.x > 0)
+            {
+                //右方向
+                enemyRigidbody.MovePosition(this.transform.position + new Vector3(1, 0, 0));
+                enemyRigidbody.MoveRotation(Quaternion.Euler(0, 0, 0));
+
+            }
+            else
+            {
+                //左方向
+                enemyRigidbody.MovePosition(this.transform.position + new Vector3(-1, 0, 0));
+                enemyRigidbody.MoveRotation(Quaternion.Euler(0, 0, 0));
+
+            }
+        }
+
     }
+
 }
---
+++ Assets/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MonoBehaviourはClassの特性上、取る
+public class GameData {
+
+    //ステージ数。いったん16
+    public static int NUMBER_OF_LEVELS = 16;
+
+    // start updateも取る。MonoBehaviourの機能なので
+
+}
